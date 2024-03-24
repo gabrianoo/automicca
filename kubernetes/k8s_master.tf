f75806d1097bbb8a673e80353d8d82c53d0610e3
Ahmed Hassanien <ahmed_hassanien@epam.com>
2024-03-24 23:57:32 +0100

    UPDATE: Added K8S master and worker VM(s) provisioning to a new files

---
+++ kubernetes/k8s_master.tf
@@ -0,0 +1,45 @@
+#===============================================================================
+# Proxmox K8S Master Resources
+#===============================================================================
+
+# Create the Kubernetes master VMs #
+resource "proxmox_vm_qemu" "master" {
+  count       = length(var.vm_master_ips)
+  name        = "${var.vm_name_prefix}-master-${count.index}"
+  target_node = var.proxmox_node
+
+  clone      = var.vm_template
+  agent      = 1
+  tags       = var.vm_tags
+
+  ciuser     = local.ssh_user
+
+  os_type    = "cloud-init"
+  sockets    = var.vm_sockets
+  cores      = var.vm_master_cores
+  vcpus      = var.vm_sockets * var.vm_master_cores
+  cpu        = "host"
+  memory     = var.vm_master_max_ram
+  balloon    = var.vm_master_min_ram
+  full_clone = var.vm_full_clone
+  onboot     = true
+
+  network {
+    model    = "virtio"
+    bridge   = var.vm_network_bridge
+  }
+
+  disk {
+    type         = var.vm_disk_type
+    size         = var.vm_master_size
+    storage      = var.vm_storage
+  }
+
+  ipconfig0    = "ip=${var.vm_master_ips[count.index]}/${var.vm_netmask},gw=${var.vm_gateway}"
+  searchdomain = var.vm_searchdomain
+  nameserver   = var.vm_dns
+
+  sshkeys = var.vm_sshkeys
+
+  depends_on = [proxmox_vm_qemu.haproxy]
+}
---
+++ kubernetes/k8s_worker.tf
@@ -0,0 +1,62 @@
+#===============================================================================
+# Proxmox K8S Worker Resources
+#===============================================================================
+
+# Create the Kubernetes worker VMs #
+resource "proxmox_vm_qemu" "worker" {
+  count       = length(var.vm_worker_ips)
+  name        = "${var.vm_name_prefix}-worker-${count.index}"
+  target_node = var.proxmox_node
+
+  clone       = var.vm_template
+  agent       = 1
+  tags        = var.vm_tags
+
+  ciuser     = var.vm_ssh_user != null ? var.vm_ssh_user : data.pass_password.ci_user.password
+  cipassword = var.vm_ssh_user_password != null ? var.vm_ssh_user_password : data.pass_password.ci_pass.password
+
+  os_type    = "cloud-init"
+  sockets    = var.vm_sockets
+  cores      = var.vm_worker_cores
+  vcpus      = var.vm_sockets * var.vm_worker_cores
+  cpu        = "host"
+  memory     = var.vm_worker_max_ram
+  balloon    = var.vm_worker_min_ram
+  full_clone = var.vm_full_clone
+  onboot     = true
+
+  network {
+    model  = "virtio"
+    bridge = var.vm_network_bridge
+  }
+
+  disk {
+    type         = var.vm_disk_type
+    size         = var.vm_worker_size
+    storage      = var.vm_storage
+  }
+
+  ipconfig0    = "ip=${var.vm_worker_ips[count.index]}/${var.vm_netmask},gw=${var.vm_gateway}"
+  searchdomain = var.vm_searchdomain
+  nameserver   = var.vm_dns
+
+  sshkeys = var.vm_sshkeys
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "cd kubespray && ansible-playbook -i ../config/hosts.ini -b -u ${self.ciuser} -e \"ansible_ssh_pass=${self.cipassword} ansible_become_pass=${self.cipassword} node=$VM_NAME delete_nodes_confirmation=yes\" -v remove-node.yml"
+    on_failure = continue
+  }
+
+  #provisioner "local-exec" {
+  #  when    = destroy
+  #  command = "sed 's/${var.vm_name_prefix}-worker-[0-9]*$//' config/hosts.ini"
+  #}
+
+  depends_on = [
+    proxmox_vm_qemu.master,
+    local_file.kubespray_hosts,
+    local_file.kubespray_k8s_cluster,
+    local_file.kubespray_all,
+  ]
+}
