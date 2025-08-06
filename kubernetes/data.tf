7fb87cc1af72be12f38c92eb44288cd7c12d4d1e
Ahmed Hassanien <ahmed.hassanien@tesco.com>
2025-08-06 14:10:36 +0200

    UPDATE: Moving the old template file engine to the new one since the old engine was archived bt terraform.

--- kubernetes/data.tf
+++ kubernetes/data.tf
@@ -22,126 +22,87 @@ data "pass_password" "ci_pass" {
 # Template files
 #===============================================================================
 
-# Kubespray all.yml template #
-data "template_file" "kubespray_all" {
-  template = file("templates/kubespray_all.tpl")
-
-  vars = {
+locals {
+  # Kubespray all.yml template #
+  kubespray_all_rendered = templatefile("templates/kubespray_all.tpl", {
     loadbalancer_apiserver = var.vm_haproxy_vip
     upstream_dns_server = var.vm_dns
-  }
-}
+  })
 
-# Kubespray k8s-cluster.yml template #
-data "template_file" "kubespray_k8s_cluster" {
-  template = file("templates/kubespray_k8s_cluster.tpl")
-
-  vars = {
+  # Kubespray k8s-cluster.yml template #
+  kubespray_k8s_cluster_rendered = templatefile("templates/kubespray_k8s_cluster.tpl", {
     kube_version        = var.k8s_version
     kube_network_plugin = var.k8s_network_plugin
     weave_password      = var.k8s_weave_encryption_password
     k8s_dns_mode        = var.k8s_dns_mode
-  }
-}
+  })
 
-# HAProxy hostname and ip list template #
-data "template_file" "haproxy_hosts" {
-  count    = length(var.vm_haproxy_ips)
-  template = file("templates/ansible_hosts.tpl")
+  # HAProxy hostname and ip list template #
+  haproxy_hosts_rendered = [
+    for i in range(length(var.vm_haproxy_ips)) : templatefile("templates/ansible_hosts.tpl", {
+      hostname = "${var.vm_name_prefix}-haproxy-${i}"
+      host_ip  = var.vm_haproxy_ips[i]
+    })
+  ]
 
-  vars = {
-    hostname = "${var.vm_name_prefix}-haproxy-${count.index}"
-    host_ip  = var.vm_haproxy_ips[count.index]
-  }
-}
+  # Kubespray master hostname and ip list template #
+  kubespray_hosts_master_rendered = [
+    for i in range(length(var.vm_master_ips)) : templatefile("templates/ansible_hosts.tpl", {
+      hostname = "${var.vm_name_prefix}-master-${i}"
+      host_ip  = var.vm_master_ips[i]
+    })
+  ]
 
-# Kubespray master hostname and ip list template #
-data "template_file" "kubespray_hosts_master" {
-  count    = length(var.vm_master_ips)
-  template = file("templates/ansible_hosts.tpl")
+  # Kubespray worker hostname and ip list template #
+  kubespray_hosts_worker_rendered = [
+    for i in range(length(var.vm_worker_ips)) : templatefile("templates/ansible_hosts.tpl", {
+      hostname = "${var.vm_name_prefix}-worker-${i}"
+      host_ip  = var.vm_worker_ips[i]
+    })
+  ]
 
-  vars = {
-    hostname = "${var.vm_name_prefix}-master-${count.index}"
-    host_ip  = var.vm_master_ips[count.index]
-  }
-}
+  # HAProxy hostname list template #
+  haproxy_hosts_list_rendered = [
+    for i in range(length(var.vm_haproxy_ips)) : templatefile("templates/ansible_hosts_list.tpl", {
+      hostname = "${var.vm_name_prefix}-haproxy-${i}"
+    })
+  ]
 
-# Kubespray worker hostname and ip list template #
-data "template_file" "kubespray_hosts_worker" {
-  count    = length(var.vm_worker_ips)
-  template = file("templates/ansible_hosts.tpl")
+  # Kubespray master hostname list template #
+  kubespray_hosts_master_list_rendered = [
+    for i in range(length(var.vm_master_ips)) : templatefile("templates/ansible_hosts_list.tpl", {
+      hostname = "${var.vm_name_prefix}-master-${i}"
+    })
+  ]
 
-  vars = {
-    hostname = "${var.vm_name_prefix}-worker-${count.index}"
-    host_ip  = var.vm_worker_ips[count.index]
-  }
-}
+  # Kubespray worker hostname list template #
+  kubespray_hosts_worker_list_rendered = [
+    for i in range(length(var.vm_worker_ips)) : templatefile("templates/ansible_hosts_list.tpl", {
+      hostname = "${var.vm_name_prefix}-worker-${i}"
+    })
+  ]
 
-# HAProxy hostname list template #
-data "template_file" "haproxy_hosts_list" {
-  count    = length(var.vm_haproxy_ips)
-  template = file("templates/ansible_hosts_list.tpl")
-
-  vars = {
-    hostname = "${var.vm_name_prefix}-haproxy-${count.index}"
-  }
-}
-
-# Kubespray master hostname list template #
-data "template_file" "kubespray_hosts_master_list" {
-  count    = length(var.vm_master_ips)
-  template = file("templates/ansible_hosts_list.tpl")
-
-  vars = {
-    hostname = "${var.vm_name_prefix}-master-${count.index}"
-  }
-}
-
-# Kubespray worker hostname list template #
-data "template_file" "kubespray_hosts_worker_list" {
-  count    = length(var.vm_worker_ips)
-  template = file("templates/ansible_hosts_list.tpl")
-
-  vars = {
-    hostname = "${var.vm_name_prefix}-worker-${count.index}"
-  }
-}
-
-# HAProxy template #
-data "template_file" "haproxy" {
-  template = file("templates/haproxy.tpl")
-
-  vars = {
+  # HAProxy template #
+  haproxy_rendered = templatefile("templates/haproxy.tpl", {
     bind_ip = var.vm_haproxy_vip
-  }
-}
+  })
 
-# HAProxy server backend template #
-data "template_file" "haproxy_backend" {
-  count    = length(var.vm_master_ips)
-  template = file("templates/haproxy_backend.tpl")
+  # HAProxy server backend template #
+  haproxy_backend_rendered = [
+    for i in range(length(var.vm_master_ips)) : templatefile("templates/haproxy_backend.tpl", {
+      prefix_server     = var.vm_name_prefix
+      backend_server_ip = var.vm_master_ips[i]
+      count             = i
+    })
+  ]
 
-  vars = {
-    prefix_server     = var.vm_name_prefix
-    backend_server_ip = var.vm_master_ips[count.index]
-    count             = count.index
-  }
-}
-
-# Keepalived master template #
-data "template_file" "keepalived_master" {
-  template = file("templates/keepalived_master.tpl")
-
-  vars = {
+  # Keepalived master template #
+  keepalived_master_rendered = templatefile("templates/keepalived_master.tpl", {
     virtual_ip = var.vm_haproxy_vip
-  }
-}
+  })
 
-# Keepalived slave template #
-data "template_file" "keepalived_slave" {
-  template = file("templates/keepalived_slave.tpl")
-
-  vars = {
+  # Keepalived slave template #
+  keepalived_slave_rendered = templatefile("templates/keepalived_slave.tpl", {
     virtual_ip = var.vm_haproxy_vip
-  }
+  })
 }
